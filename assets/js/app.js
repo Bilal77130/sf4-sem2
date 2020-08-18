eb83c9744d525cb32c82f5fd1ff9c6447daa94f7
BilalGHULAM <bilal.ghulam.0123@gmail.com>
2020-08-18 09:42:14 +0200

    Intalle WEB PACK ENCORE

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// Importer le fichier SCSS de l'entré app
+require('../css/app.scss');
+
+// Importer le script pour le menu mobile
+
+require('./menu.js')
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
